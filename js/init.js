7481d1765774158b7601c079fc878cb6ae2adefc
tino415 <cernakmartin3@gmail.com>
2015-10-10 16:41:59 +0200

    Paralax and external files + scroll reduce

---
+++ js/init.js
@@ -0,0 +1,58 @@
+$(function () {
+    var $headerNav = $('#header-navbar');
+    var $transparentFadeIn = $('.transparent-fadeIn');
+    var $body = $('body');
+
+    $(window).scroll(function () {
+        var pos = $(window).scrollTop();
+
+        if (pos > 300) {
+            $headerNav.css('visibility', 'visible');
+            $headerNav.css('opacity', '1');
+        } else {
+            $headerNav.css('visibility', 'hidden');
+            $headerNav.css('opacity', '0');
+        }
+
+        $transparentFadeIn.each(function() {
+            var bottomOfObject = $(this).offset().top + 100;
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+            if (bottomOfWindow > bottomOfObject) {
+                $(this).animate({opacity: '1'}, 500);
+            }
+        });
+
+        $('#sidebar').css('visibility', pos < $(window).height() ? 'hidden' : 'visible');
+    });
+
+    //$('.hidden-before-load').hide();
+
+    $('#project-name').css('opacity', '1');
+    setTimeout(function () {
+        $('#project-title').css('opacity', '1');
+    }, 1000);
+    setTimeout(function () {
+        $('#project-links').css('opacity', '1');
+    }, 1000);
+    setTimeout(function () {
+        $('.circle-button').css('opacity', '1');
+    }, 1000);
+
+
+    $('a[href*=#]').click(function (event) {
+        var href = $(this).attr('href');
+
+        var $target = (href.indexOf('#', href.length - 1)) ? $($(this).attr('href')) : $body;
+
+        $('html, body').animate({
+            scrollTop: $target.offset().top
+        }, 1000);
+
+        event.preventDefault();
+    });
+
+    $.stellar();
+    $("[data-toggle=tooltip]").tooltip();
+    $body.scrollspy({target: '#header-navbar'})
+});
